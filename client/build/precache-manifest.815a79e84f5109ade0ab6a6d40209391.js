09a3e66c02c4d47cbc7468ca6271eaa158310abf
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2020-06-19 09:33:07 -0600

    npm build

---
+++ client/build/precache-manifest.815a79e84f5109ade0ab6a6d40209391.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b649c5e2515ef2be719b",
+    "url": "/static/css/main.a27843fa.chunk.css"
+  },
+  {
+    "revision": "b649c5e2515ef2be719b",
+    "url": "/static/js/main.4ddb66e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "668022f364276fab8686",
+    "url": "/static/js/2.97a16831.chunk.js"
+  },
+  {
+    "revision": "207e6d043e42d8bd43e4f341d6139987",
+    "url": "/index.html"
+  }
+];
